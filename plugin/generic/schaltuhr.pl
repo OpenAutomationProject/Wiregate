096f60bc0fb4d22d8cfe9bda614e223b0dfbda51
swiss1 <swiss1@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2011-07-03 18:35:33 +0000

    Es wurde die Berechnung der Kalenderwoche ergänzt, da diese nicht über localtime ausgegeben wird. Das Plugin wurde gründlich auf Funktion getestet. Version 0.4 BETA

--- plugin/generic/schaltuhr.pl
+++ plugin/generic/schaltuhr.pl
@@ -1,5 +1,5 @@
 # Plugin zum Zeit abhängigem schaten von GA's (Schaltuhr)
-# Version 0.3 BETA 03.07.2011
+# Version 0.4 BETA 03.07.2011
 # Copyright: swiss (http://knx-user-forum.de/members/swiss.html)
 # License: GPL (v2)
 # Aufbau möglichst so, dass man unterhalb der Einstellungen nichts verändern muss!
@@ -12,40 +12,44 @@ my @Schaltzeiten;
 
 #Pro Schaltpunkt einfach den unten stehenden Eintrag kopieren und anpassen.
 
-push @Schaltzeiten, { name => "bewässerung_ein", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 15, Minuten => 45, Wert => 1, DPT => 1, ga => '2/0/0', KW => '', Monat => '' };
+push @Schaltzeiten, { name => "bewässerung_ein", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 20, Minuten => 30, Wert => 1, DPT => 1, ga => '2/0/0', KW => '', Monat => '' };
 
-push @Schaltzeiten, { name => "bewässerung_aus", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 15, Minuten => 46, Wert => 0, DPT => 1, ga => '2/0/0', KW => '', Monat => '' };
+push @Schaltzeiten, { name => "bewässerung_aus", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 20, Minuten => 31, Wert => 0, DPT => 1, ga => '2/0/0', KW => '', Monat => '' };
 
 ######################
 ##ENDE Einstellungen##
 ######################
 
 use POSIX;
+use Time::Local;
 
-# Eigenen Aufruf-Zyklus auf 30sek. setzen
-$plugin_info{$plugname.'_cycle'} = 30;
+# Eigenen Aufruf-Zyklus auf 20sek. setzen
+$plugin_info{$plugname.'_cycle'} = 20;
 
 #Hier wird ein Array angelegt, um die Wochentagsnummer von localtime zu übersetzen
 my @Wochentag = ('sonntag', 'montag', 'dienstag', 'mittwoch', 'donnerstag', 'freitag', 'samstag');
 
-my $sec;
-my $min;
-my $hour;
-my $mday;
-my $mon;
-my $year;
-my $wday;
-my $yday;
+my $sec; #Sekunde
+my $min; # Minute
+my $hour; #Stunde
+my $mday; #Monatstag
+my $mon; #Monatsnummer
+my $year; #Jahr
+my $wday; #Wochentag 0-6
+my $yday; #Tag ab 01.01.xxxx
 my $isdst;
 
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+$year += 1900;
+$mon += 1;
+
+my $kw = getWeekNumber($year, $mon, $mday);
+$plugin_info{$plugname.'_debug'} = $kw;
 
 foreach my $element (@Schaltzeiten) {
 
-	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-	$year += 1900;
-
 	if ($element->{$Wochentag[$wday]} == 1 && $element->{Stunden} == $hour && $element->{Minuten} == $min && $element->{KW} ne '') {
-		if ($element->{KW} == $yday) {
+		if ($element->{KW} == $kw) {
 			knx_write($element->{ga},$element->{Wert},$element->{DPT});
 			next;
 		} else {
@@ -63,4 +67,68 @@ foreach my $element (@Schaltzeiten) {
 		next;
 	}	
 next;
+}
+
+sub getWeekNumber {
+my ($year, $month, $day) = @_;
+my $weekNumber = checkWeekNumber($year, $month, $day);
+# wenn Wochennummer gleich 0, dann ist das aktuelle Datum
+# in der Woche vor dem 4. Januar
+# also in der letzten Woche des letzten Jahres
+if ($weekNumber eq 0)
+{
+# Wochennummer des letzten Woche des letzten Jahres suchen
+$weekNumber = checkWeekNumber(($year - 1), 12, 31);
+
+# wenn die Wochennummer größer als 52 ist
+# dann prüfen ob diese Wochennummer korrekt ist oder
+# sie bereits die erste Woche des aktuellen Jahres ist
+if ($weekNumber gt 52)
+{
+$weekNumber = checkWeekNumber($year, 1, 1);
+# wenn der 1. Januar des aktuellen Jahres in der Woche 0 liegt
+# dann ist es die Woche 53
+if ($weekNumber eq 0)
+{
+$weekNumber = 53;
+}
+}
+}
+# wenn die Wochennummer größer als 52 ist
+# dann prüfen ob diese Wochennummer korrekt ist oder
+# sie bereits die erste Woche des nächsten Jahres ist
+elsif ($weekNumber gt 52)
+{
+$weekNumber = checkWeekNumber(($year + 1), 1, 1);
+# wenn der 1. Januar des nächsten Jahres in der Woche 0 liegt
+# dann ist es die Woche 53
+if ($weekNumber eq 0)
+{
+$weekNumber = 53;
+}
+}
+
+return ($weekNumber);
+}
+
+sub checkWeekNumber {
+my ($year, $month, $day) = @_;
+
+# 4. Januar als erste Woche erstellen
+my $firstDateTime = timelocal(0, 0, 12, 4, 0, $year);
+# Wochentag des 4. Januar ermitteln
+my $dayOfWeek = (localtime($firstDateTime))[6];
+$dayOfWeek = abs((($dayOfWeek + 6) % 7));
+# geh zu Wochenanfang (Montag) zurück
+$firstDateTime -= ($dayOfWeek * 3600 * 24);
+
+# aktuelles Datum erstellen
+my $currentDateTime = timelocal(0, 0, 14, $day, ($month - 1),$year);
+
+# Differenz in Tagen berechnen
+my $diffInDay = ($currentDateTime - $firstDateTime) / 3600 / 24;
+# Anzahl der Wochen zwischen aktuellem Datum und 4. Januar berechnen
+my $weekNumber = floor($diffInDay / 7) + 1;
+
+return ($weekNumber);
 }
