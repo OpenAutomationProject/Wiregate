8493ad2325514c5ab9272eb807ce44bb45486d3c
swiss1 <swiss1@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2011-07-03 14:04:15 +0000

    Das einstellen der DPT wurde hinzugefügt. Damit können jetzt auch Werte zeitabhängig gesendet werden. Version 0.2

--- plugin/generic/schaltuhr.pl
+++ plugin/generic/schaltuhr.pl
@@ -1,5 +1,5 @@
-# Plugin zum Zeit abhängigem schaten von beliebig vielen GA's (Schaltuhr)
-# Version 0.1 03.07.2011
+# Plugin zum Zeit abhängigem schaten von GA's (Schaltuhr)
+# Version 0.2 03.07.2011
 # Copyright: swiss (http://knx-user-forum.de/members/swiss.html)
 # License: GPL (v2)
 # Aufbau möglichst so, dass man unterhalb der Einstellungen nichts verändern muss!
@@ -12,9 +12,9 @@ my @Schaltzeiten;
 
 #Pro Schaltpunkt einfach den unten stehenden Eintrag kopieren und anpassen.
 
-push @Schaltzeiten, { name => "bewässerung_ein", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 15, Minuten => 45, Befehl => 1, ga => '2/0/0' };
+push @Schaltzeiten, { name => "bewässerung_ein", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 15, Minuten => 45, Wert => 1, DPT => 1, ga => '2/0/0' };
 
-push @Schaltzeiten, { name => "bewässerung_aus", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 15, Minuten => 46, Befehl => 0, ga => '2/0/0' };
+push @Schaltzeiten, { name => "bewässerung_aus", montag => 1, dienstag => 1, mittwoch => 1, donnerstag => 1, freitag => 1, samstag => 0, sonntag => 1, Stunden => 15, Minuten => 46, Wert => 0, DPT => 1, ga => '2/0/0' };
 
 ######################
 ##ENDE Einstellungen##
@@ -45,7 +45,7 @@ foreach my $element (@Schaltzeiten) {
 	$year += 1900;
 
 	if ($element->{$Wochentag[$wday]} == 1 && $element->{Stunden} == $hour && $element->{Minuten} == $min) {
-		knx_write($element->{ga},$element->{Befehl},1);
+		knx_write($element->{ga},$element->{Wert},$element->{DPT});
 	}
 next;
 }
