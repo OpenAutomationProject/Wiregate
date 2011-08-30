87ec044581d35e0a430bc1ef5a181d87eff2c5d4
cschleiffer <cschleiffer@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2011-08-30 07:28:00 +0000

    v0.2 of Kaco datalogger plugin

--- wiregate/plugin/generic/Datenlogger-PV-Kaco-Prolog.pl
+++ wiregate/plugin/generic/Datenlogger-PV-Kaco-Prolog.pl
@@ -1,29 +1,37 @@
 # Monitoring a PV system with Kaco ProLOG
-# v0.1
+# v0.2
+# License: GPL v2
 
 use LWP::Simple;
+use HTML::TableExtract; # get it with 'apt-get install libhtml-tableextract-perl'
 
 # Configuration 
 # ****************************
 
-my $myProLogIP       = "192.168.178.116";  # IP address of Kaco ProLOG
-my $myCurrentPowerGA = "0/4/236";          # GA for sending current power [W], DPT 14.056
+my $myProLogIP          = "192.168.178.116";	# IP address of Kaco ProLOG
+my $myCurrentPowerGA    = "0/4/236";		# GA for sending current power [W], DPT 14.056
+my $myEnergyTodayGA     = "0/4/233";		# GA for sending energy produced this day [Wh], DPT 13.010
+my $myEneryYesterdayGA  = "";
+my $myEnergyThisMonthGA = "0/4/234";		# GA for sending energy produced this month [kWh], DPT 13.013
+my $myEnergyThisYearGA  = "0/4/235";		# GA for sending energy produced this year [Wh], DPT 13.013
+my $myEnergyTotalGA     = "0/4/230";		# GA for sending total accumulated enery production [kWh], DPT 13.013
 # ToDo
-my $myEnergy15minGA  = "0/4/231";          # GA for sending energy produced in last 15min [Wh], DPT 13.010
-my $myEnergy60minGA  = "0/4/232";          # GA for sending energy produced in last 60min [Wh], DPT 13.010
+my $myEnergy15minGA     = "0/4/231";		# GA for sending energy produced in last 15min [Wh], DPT 13.010
+my $myEnergy60minGA     = "0/4/232";		# GA for sending energy produced in last 60min [Wh], DPT 13.010
+
 
 # Arrays to hold GAs for detailed values from the single power converters
 # Use their RS485 bus address as array index
 # If a GA is left empty, it is not send to the bus
-my @myDcVoltageGA;         # DC voltage in [V], DPT 14.027
-my @myDcCurrentGA;        # DC current in [A], DPT 14.019
-my @myDcPowerGA;        # DC power [W], DPT 13.010
-my @myAcVoltageGA;        # AC voltage in [V], DPT 14.027
-my @myAcCurrentGA;        # AC current in [A], DPT 14.019
-my @myAcPowerGA;        # AC power in [W], DPT 13.010
-my @myAcDailyEnergyGA;        # Energy output, daily accumulation in [kWh], DPT 13.013
-my @myTemperatureGA;        # Temperature of inverter in [°C], DPT 9.001
-my @myStatusGA;            # Status of inverter, DPT 5.010
+my @myDcVoltageGA; 		# DC voltage in [V], DPT 14.027
+my @myDcCurrentGA;		# DC current in [A], DPT 14.019
+my @myDcPowerGA;		# DC power [W], DPT 13.010
+my @myAcVoltageGA;		# AC voltage in [V], DPT 14.027
+my @myAcCurrentGA;		# AC current in [A], DPT 14.019
+my @myAcPowerGA;		# AC power in [W], DPT 13.010
+my @myAcDailyEnergyGA;		# Energy output, daily accumulation in [kWh], DPT 13.013
+my @myTemperatureGA;		# Temperature of inverter in [°C], DPT 9.001
+my @myStatusGA;			# Status of inverter, DPT 5.010
 
 # WR1 Powador 6002
 $myDcVoltageGA[1]     = "0/4/237";
@@ -53,6 +61,7 @@ $myStatusGA[2]        = "0/4/254";
 
 my $mySummaryDataDownloadPath = "/html/de/locale_live_standard.html";
 my $myDetailedDataDownloadPath = "/get_online_wr.cgi?q=U_DC_0;I_DC_0;P_DC_WR;U_AC_0;I_AC_0;P_AC_WR;T_WR;E_D_WR;S";
+my $myWrOverviewPath = "/html/de/onlineOverWr.html";
 my @myWRStatusMsg;
 
 $myWRStatusMsg[ 0] = 'WR gerade eingeschaltet';
@@ -105,9 +114,7 @@ $myOverview[0] *= 1000;
 knx_write( $myCurrentPowerGA , $myOverview[0], 14 );
 update_rrd("PV", "P_total", $myOverview[0] );
 
-
 # more detailed data are requested here
-
 $content = get( "http://$myProLogIP$myDetailedDataDownloadPath" );
 return "Detail-Webabfrage fehlgeschlagen" unless defined $content;
 
@@ -115,7 +122,7 @@ my @myDetails = split( /\n/, $content );
 my $myWRStatusChanges = '';
 
 for my $i (2 .. $#myDetails - 1) {
-    $myDetails[$i] =~ s/\r//g;    # remove carrige return
+    $myDetails[$i] =~ s/\r//g;	# remove carrige return
     my @myWRDetails = split( /;/, $myDetails[$i] );
     
     # only process if system supplies values
@@ -136,7 +143,7 @@ for my $i (2 .. $#myDetails - 1) {
         update_rrd( 'PV', "Temperature_WR$myWRDetails[1]", $myWRDetails[8] );
         if( $myTemperatureGA[ $myWRDetails[1] ] ) { knx_write( $myTemperatureGA[ $myWRDetails[1] ], $myWRDetails[8], 9 ); }
         update_rrd( 'PV', "DailyEnergy_WR$myWRDetails[1]", $myWRDetails[9] / 1000 );
-        if( $myAcDailyEnergyGA[ $myWRDetails[1] ] ) { knx_write( $myAcDailyEnergyGA[ $myWRDetails[1] ], $myWRDetails[9]/1000, 13 ); }    
+        if( $myAcDailyEnergyGA[ $myWRDetails[1] ] ) { knx_write( $myAcDailyEnergyGA[ $myWRDetails[1] ], $myWRDetails[9]/1000, 13 ); }	
         update_rrd( 'PV', "Status_WR$myWRDetails[1]", $myWRDetails[10] );
         if( $myStatusGA[ $myWRDetails[1] ] ) { knx_write( $myStatusGA[ $myWRDetails[1] ], $myWRDetails[10], 5 ); }
             
@@ -147,5 +154,33 @@ for my $i (2 .. $#myDetails - 1) {
         }
     }
 }
-return $myWRStatusChanges;
 
+
+$content = get( "http://$myProLogIP$myWrOverviewPath" );
+return "Übersichts-Webabfrage fehlgeschlagen" unless defined $content;
+
+my $tables = HTML::TableExtract->new( );
+$tables->parse($content);
+
+my $lastElement = "";
+foreach my $table ($tables->tables) {
+    foreach my $rowCols ($table->rows) {
+        foreach my $col (@$rowCols) {
+            if( $lastElement eq "Aktuelle Monatsenergie" ) {
+                knx_write( $myEnergyThisMonthGA , $col, 13 );
+            } elsif ( $lastElement eq "Aktuelle Tagesenergie" ) {
+                knx_write( $myEnergyTodayGA , $col, 13 );
+            } elsif ( $lastElement eq "Aktuelle Jahresenergie" ) {
+                knx_write( $myEnergyThisYearGA , $col, 13 );
+            } elsif ( $lastElement eq "Tagesenergie Vortag" ) {
+                knx_write( $myEneryYesterdayGA , $col, 13 );
+            } elsif ( $lastElement eq "Gesamtenergie" ) {
+                knx_write( $myEnergyTotalGA , $col, 13 );
+            }
+            $lastElement = $col;
+        }
+    }
+}
+
+
+return $myWRStatusChanges;
