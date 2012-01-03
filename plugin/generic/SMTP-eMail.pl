b8df56e09052a01af584076c04b61674603e6eb2
makki1 <Makki1@users.noreply.github.com>
2012-01-03 15:39:57 +0000

    WireGate-Plugins: SMTP revisited.. fix multiple recipients

--- plugin/generic/SMTP-eMail.pl
+++ plugin/generic/SMTP-eMail.pl
@@ -2,7 +2,8 @@
 # das macht nichts sinnvolles, sendet jede Stunde ein eMail,
 # soll nur als Vorlage dienen
 # - mit SSL (alte Version ohne SSL sieh SVN rev 622)
-# - benoetigt Paket libnet-smtp-ssl-perl
+# - benoetigt Paket libnet-smtp-ssl-perl libauthen-sasl-perl
+# (Updates->Pakete installieren oder apt-get install ..)
 
 # Aufbau moeglichst so, dass man unterhalb der Definitionen nichts aendern muss!
 
@@ -17,7 +18,7 @@ my $Absender = 'WireGate <abc@gmx.de>'; # unbedingt anpassen, die Absenderadress
 my $Betreff = "eMail von $hostname";
 my $text = "email-Body";
 my $username = 'abc@gmx.de'; #Anpassen! Username fuer SMTP-Server
-my $password = "meinpasswort"; #Anpassen! Passwort fuer SMTP-Server
+my $password = 'meinpasswort'; #Anpassen! Passwort fuer SMTP-Server
 my $mailserver='mail.gmx.net:465'; # SMTP-Relay mit SSL: das muss natuerlich angepasst werden!
 # oder z.B. smtp.gmail.com:465 fuer Gmail; 
 $plugin_info{$plugname.'_cycle'} = 3600;
@@ -33,7 +34,7 @@ my $smtp = Net::SMTP::SSL->new($mailserver, Timeout => 10) or return "Fehler bei
 $smtp->auth($username,$password) or return "SASL Auth failed $!;$@"; # try SASL
 $smtp->status() < 5 or return "Auth failed: $!; $@ ". $smtp->status();
 $smtp->mail($Absender) or return "Absender $Absender abgelehnt $!";
-$smtp->to($Empfaenger) or return "Empfaenger $Empfaenger abgelehnt $!"; 
+$smtp->to(split(',',$Empfaenger)) or return "Empfaenger $Empfaenger abgelehnt: $!"; 
 $smtp->data() or return "Data failed $!";
 $smtp->datasend("To: $Empfaenger\n") or return "Empfanger $Empfaenger (Header-To) abgelehnt $!";
 $smtp->datasend("Subject: $Betreff\n") or return "Subject $Betreff abgelehnt $!";
