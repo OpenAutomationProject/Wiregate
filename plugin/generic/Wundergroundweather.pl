1d534fb84fbba5f1ff43647d482e670c318d5a8a
makki1 <Makki1@users.noreply.github.com>
2012-11-29 16:01:34 +0000

    Remove BOM, CRLF, umlaute in HTML

--- plugin/generic/Wundergroundweather.pl
+++ plugin/generic/Wundergroundweather.pl
@@ -1,8 +1,8 @@
-﻿# Plugin zum Abfragen und Darstellen von
+# Plugin zum Abfragen und Darstellen von
 # WundergroundWeather von jensgulow
 # based on
 # Plugin zum Abfragen von Google Weather
-# Version 0.5 2011-02-15
+# Version 0.51 2012-11-29
 # Copyright: Bodo (http://knx-user-forum.de/members/bodo.html)
 # License: GPL (v2)
 
@@ -10,30 +10,30 @@
 ### DEFINITION ###
 ##################
 
-my $city 						= "Leipzig";		# Meine Stadt, hier statt ü,ä,ö einfach u,a,o nehmen oder ue,ae,oe
-my $country 					= "Germany";		# Mein Land
-my $lang 						= "DL";				# Meine Sprache
-my $api 						= "xxxxxxx";		# API, muss man sich bei Wunderground besorgen
+my $city = "Leipzig";        # Meine Stadt, hier statt &uuml;,&auml;,&ouml; einfach u,a,o nehmen oder ue,ae,oe
+my $country = "Germany";        # Mein Land
+my $lang = "DL";                # Meine Sprache
+my $api = "xxxxxxx";        # API, muss man sich bei Wunderground besorgen
 
-my $wunderground_temp_ga							= "10/0/4";		# Gruppenadresse Temperatur (DPT9.001)
-my $wunderground_hum_ga								= "10/0/5";		# Gruppenadresse Luftfeuchte (DPT9.007)
-my $wunderground_forecast_maxtemp_ga 				= "10/0/6";		# Gruppenadresse Temperatur Maximum (DPT9.001)
-my $wunderground_forecast_mintemp_ga 				= "10/0/7";		# Gruppenadresse Temperatur Minimum (DPT9.001)
-my $wunderground_clouds_ga							= "10/0/8";		# Gruppenadresse Wolken (DPT16)
-my $wunderground_forecast_clouds_ga					= "10/0/9";		# Gruppenadresse Vorhersage Wolken (DPT16)
-my $wunderground_wind_ga							= "10/0/10";	# Gruppenadresse Wind (DPT16)
+my $wunderground_temp_ga = "10/0/4";        # Gruppenadresse Temperatur (DPT9.001)
+my $wunderground_hum_ga = "10/0/5";        # Gruppenadresse Luftfeuchte (DPT9.007)
+my $wunderground_forecast_maxtemp_ga = "10/0/6";        # Gruppenadresse Temperatur Maximum (DPT9.001)
+my $wunderground_forecast_mintemp_ga = "10/0/7";        # Gruppenadresse Temperatur Minimum (DPT9.001)
+my $wunderground_clouds_ga = "10/0/8";        # Gruppenadresse Wolken (DPT16)
+my $wunderground_forecast_clouds_ga = "10/0/9";        # Gruppenadresse Vorhersage Wolken (DPT16)
+my $wunderground_wind_ga = "10/0/10";    # Gruppenadresse Wind (DPT16)
 
-my $wunderground_ip									= "http://api.wunderground.com/api/";
-my $symbole											= "/symbole/";						# Pfad zu den Wettersymbolen
-my $symbolebg										= "/symbolebg/";					# Pfad zu den Wetterhintergründen
-my $htdocs											= "/var/www/";						# Das Webverzeichnis
-my $wunderground_xml								= "wunderground_weather.xml";		# Der XML Datensatz
-my $weather_html									= "wunderground_weather.html";		# Ausgabe als HTML
-my $wunderground_css								= "wunderground_weather.css";		# Das Stylesheet
+my $wunderground_ip                                    = "http://api.wunderground.com/api/";
+my $symbole                                            = "/symbole/";                        # Pfad zu den Wettersymbolen
+my $symbolebg                                        = "/symbolebg/";                    # Pfad zu den Wetterhintergr&uuml;nden
+my $htdocs                                            = "/var/www/";                        # Das Webverzeichnis
+my $wunderground_xml                                = "wunderground_weather.xml";        # Der XML Datensatz
+my $weather_html                                    = "wunderground_weather.html";        # Ausgabe als HTML
+my $wunderground_css                                = "wunderground_weather.css";        # Das Stylesheet
 
-$plugin_info{$plugname.'_cycle'} = 1800;	# Eigenen Aufruf-Zyklus setzen (Initialisierung/zyklisches prüfen) 
-						# nicht zu klein, da die Daten sowieso in längeren Perioden refresht werden
-						# und das Plugin auf die CF schreibt.
+$plugin_info{$plugname.'_cycle'} = 1800;    # Eigenen Aufruf-Zyklus setzen (Initialisierung/zyklisches pr&uuml;fen) 
+                        # nicht zu klein, da die Daten sowieso in l&auml;ngeren Perioden refresht werden
+                        # und das Plugin auf die CF schreibt.
 
 #######################
 ### ENDE DEFINITION ###
@@ -49,10 +49,10 @@ use Encode qw(encode decode);
 my $url = $wunderground_ip.$api."/conditions/forecast/astronomy/lang:".$lang."/q/".$country."\/".$city."\.xml";
 #my $xml = encode("utf8",get($url));
 my $xml = get($url);
-die "Couldn't get it!" unless defined $xml;
+return "Couldn't get it!" unless defined $xml;
 
 my $xml_w = ">".$htdocs.$wunderground_xml;
-open(XML, $xml_w);    # XML Datei zum Schreiben öffnen
+open(XML, $xml_w);    # XML Datei zum Schreiben &ouml;ffnen
 print XML $xml;
 close(XML);
 
@@ -89,7 +89,7 @@ if ($wunderground_forecast_clouds =~ m/(\D*)(\s)(\D*)/) { # ($1)($2)($3)
 }
 my $wunderground_wind = $weather->{current_observation}->{wind_string};
 if ($wunderground_wind =~ m/(\D*)(\s)(\D*)/) { # ($1)($2)($3)
-knx_write($wunderground_wind_ga,$1." ".$3,"16");
+  knx_write($wunderground_wind_ga,$1." ".$3,"16");
 } else {
   knx_write($wunderground_wind_ga,$wunderground_wind,16);
 }
@@ -105,18 +105,19 @@ my $html =
 <title>Wetter</title>
 <meta name='language' content='de'>
 <meta http-equiv='content-type' content='text/html; charset=utf8'>
+<meta http-equiv='refresh' content='". $plugin_info{$plugname.'_cycle'}/4 ."'>
 <link rel='stylesheet' type='text/css' href='".$wunderground_css."'>
 <style type='text/css'>
 <!--
 
 table
 {
-	border: 1px;
-	border-radius: 10px;
-	-moz-border-radius: 10px;
-  	-webkit-border-radius: 10px;
-  	-o-border-radius: 10px;
-	overflow: hidden;
+    border: 1px;
+    border-radius: 10px;
+    -moz-border-radius: 10px;
+      -webkit-border-radius: 10px;
+      -o-border-radius: 10px;
+    overflow: hidden;
 }
 
 td
@@ -126,9 +127,9 @@ td
   font-size: 16px;
   text-shadow:black 3px 2px;
   border-radius: 10px;
-	-moz-border-radius: 10px;
-  	-webkit-border-radius: 10px;
-  	-o-border-radius: 10px;
+    -moz-border-radius: 10px;
+      -webkit-border-radius: 10px;
+      -o-border-radius: 10px;
   overflow: hidden;
   padding: 10px;
   margin:0;
@@ -163,9 +164,9 @@ h3
 
 "<tr height=380px>\n".
 "<td width=250px align=left>\n".
-"<h2 align=center>".$weather->{current_observation}->{temp_c}."°</h2><h3><br/>\n".
-"Gefühlt: ".$weather->{current_observation}->{feelslike_c}."° C<br/>\n".
-"Bewölkung: ".$weather->{current_observation}->{weather}."<br/>\n".
+"<h2 align=center>".$weather->{current_observation}->{temp_c}."&deg;</h2><h3><br/>\n".
+"Gef&uuml;hlt: ".$weather->{current_observation}->{feelslike_c}."&deg; C<br/>\n".
+"Bew&ouml;lkung: ".$weather->{current_observation}->{weather}."<br/>\n".
 "Luftfeuchte: ".$weather->{current_observation}->{relative_humidity}."<br/>\n".
 "Niederschlag heute: ".$weather->{current_observation}->{precip_today_metric}." mm"."<br/>\n".
 "Windrichtung: ".$weather->{current_observation}->{wind_dir}."<br/>\n".
@@ -181,8 +182,8 @@ for(my $j=0;$j<4;$j++) {
 $html = $html."<td align=center>\n".
 "<strong>".$weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{date}->{weekday}."</strong><br/>\n"."<h3>".
 $weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{conditions}."</h3><br/>\n<h1><font color=\"FFF799\">".
-$weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{high}->{celsius}."° C</font><br/>\n".
-$weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{low}->{celsius}."° C</h1><h3><br/>\n".
+$weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{high}->{celsius}."&deg; C</font><br/>\n".
+$weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{low}->{celsius}."&deg; C</h1><h3><br/>\n".
 "Niederschlagsrisiko ".$weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{pop}."%<br/>\n</h3>".
 "<img width=150px height=150px src=\"".$symbole.$weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{icon}."\.png\" alt=\"".
 $weather->{forecast}->{simpleforecast}->{forecastdays}->{forecastday}->[$j]->{conditions}."\" /><br/>\n".
@@ -195,6 +196,7 @@ $html = $html."</tr>
 
 my $html_datei = $htdocs.$weather_html;
 
-open(HTML, ">:utf8", $html_datei);    # HTML Datei zum Schreiben öffnen
-  print HTML $html;
+open(HTML, ">:utf8", $html_datei);    # HTML Datei zum Schreiben &ouml;ffnen
+print HTML $html;
 close(HTML);
+
