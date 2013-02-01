8c6550607f6879f0bb1f4da129e305dc219a291e
JuMi2006 <JuMi2006@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2013-02-01 07:48:24 +0000

    *bugfix: timer runs over restarted wiregated now

--- wiregate/plugin/generic/Timer.pl
+++ wiregate/plugin/generic/Timer.pl
@@ -1,39 +1,37 @@
 # Timer
-# v 0.1 
-# 2012-11-23 JuMi2006 -> http://knx-user-forum.de
+# v 0.2 
+# 2013-01-31 JuMi2006 -> http://knx-user-forum.de
 
 #####################
 ### Konfiguration ###
 #####################
 
-my $ga  = "3/2/28"; 	#GA
-my $delay = 9000;          #Delay in Sekunden
-my $start_value = 1;	#Timerstart bei 1 -> 1 (Aus-Timer)
-			#Timerstart bei 0 -> 0 (An-Timer)
-			
+my $ga  = "0/0/7";     #GA
+my $delay = 900;          #Delay in Sekunden
+my $start_value = 1;    #Timerstart bei 1 -> 1 (Aus-Timer)
+            #Timerstart bei 0 -> 0 (An-Timer)
+            
 ##########################
 ### Ende Konfiguration ###
 ##########################
 
-$plugin_info{$plugname.'_cycle'} = 0;
-$plugin_subscribe{$ga}{$plugname} = 1;	
+$plugin_info{$plugname.'_cycle'} = 60;
+$plugin_subscribe{$ga}{$plugname} = 1;    
 my $val_in = knx_read($ga,300,1);
+my $time = time;
 
 if ($msg{'apci'} eq "A_GroupValue_Write" && $msg{'dst'} eq $ga)
-{	  
+{      
     if ($val_in == $start_value)   # wenn Start von GA erkannt
     {
     plugin_log($plugname,"Timer aktiv");
     $plugin_info{$plugname.'_timer_active'} = 1;# setzt eine Variable zur Fehlervermeidung
-    $plugin_info{$plugname.'_last'} = time;
-    $plugin_info{$plugname.'_cycle'} = $delay;  # Plugin-Wiederholung nach X Sekunden
+    $plugin_info{$plugname.'_last_start'} = time;
     }
     
     if ($val_in == !$start_value)   # wenn Stop von GA erkannt
-    {
-    $plugin_info{$plugname.'_last'} = time;
-    $plugin_info{$plugname.'_cycle'} = 0;  		
-    $plugin_info{$plugname.'_timer_active'} = 0;	# setzt eine Variable zur Fehlervermeidung
+    {       
+    $plugin_info{$plugname.'_timer_active'} = 0;    # setzt eine Variable zur Fehlervermeidung
     plugin_log($plugname,"Timer deaktiviert");
     }
 }
@@ -41,13 +39,11 @@ if ($msg{'apci'} eq "A_GroupValue_Write" && $msg{'dst'} eq $ga)
 else
 
 {
-    if ($plugin_info{$plugname.'_timer_active'} == 1)
+    if (($plugin_info{$plugname.'_timer_active'} == 1) && ($time > ($plugin_info{$plugname.'_last_start'} + $delay)))
     {
     knx_write($ga,!$start_value,1);                 # negiert senden
     $plugin_info{$plugname.'_timer_active'} = 0;    # setzt eine Variable zur Fehlervermeidung
     plugin_log($plugname,"Timer abgelaufen");
-    $plugin_info{$plugname.'_last'} = time;
-    $plugin_info{$plugname.'_cycle'} = 0;
     }
 }
 
