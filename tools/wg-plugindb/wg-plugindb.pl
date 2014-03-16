764733d02515faf309e63832b4534651e0722587
chris_ace <ctr49@users.noreply.github.com>
2014-03-16 11:12:52 +0000

    added wg-plugindb.pl initial version from http://knx-user-forum.de/252600-post40.html Make plugin-db (/tmp/wiregate_plugin.db) available with JSON output for usage by CV "wgplugin_info"

---
+++ tools/wg-plugindb/wg-plugindb.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+use DB_File;
+use Fcntl;
+use strict;
+
+my $plugin_infofile = "/tmp/wiregate_plugin.db";
+my %plugin_info;
+tie %plugin_info, "DB_File", $plugin_infofile, O_RDONLY, 0666, $DB_HASH
+ or die "Cannot open file 'plugin_info': $!";
+
+print "Content-Type: text/plain\n\n";
+
+print "{ ";
+# Split query-string into name/value pairs
+my @pairs = split(/&/, $ENV{'QUERY_STRING'});
+my $sep ='';
+
+foreach my $pair (@pairs)
+{
+    my ($name, $value) = split(/=/, $pair);
+    if ($name eq "name" && defined($plugin_info{$value}) ) {
+        print "$sep \"$value\" : \"$plugin_info{$value}\" ";
+        $sep = ',';
+    }
+}
+
+# end json
+print " }";
